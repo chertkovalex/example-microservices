ccd5c2b94050efe3a32833790f356dfdc58442c6
Gal Noy <gal@tikalk.com>
2016-06-05 20:30:20 +0300

    added redux

--- service-frontend/src/components/list.jsx
+++ service-frontend/src/components/list.jsx
@@ -1,39 +1,21 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 const List = (props) => (
   <ul className="collection">
-    <li className="collection-item avatar">
-      <img src="images/yuna.jpg" alt="" className="circle"/>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle">folder</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle green">insert_chart</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
-    <li className="collection-item avatar">
-      <i className="material-icons circle red">play_arrow</i>
-      <span className="title">Title</span>
-      <p>First Line <br/>
-         Second Line
-      </p>
-      <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
-    </li>
+    {
+      props.list.map((item) =>
+        <li className="collection-item avatar">
+          <img src={item.iconUrl} alt="" className="circle"/>
+          <span className="title">{item.title}</span>
+          <p>
+            {item.description}
+            <br/>
+            {item.footer}
+          </p>
+          <a href="#!" className="secondary-content"><i className="material-icons">grade</i></a>
+        </li>
+      )
+    }
   </ul>
 )
 
